19e079ddf8fabf97952389d66774c6c1354adde0
CheolWan <sam_000@naver.com>
2023-11-16 04:18:51 +0900

    remove mainpage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import { Route, Routes } from "react-router-dom";
-import Main from "./pages/Main.js";
 import Weather from "./pages/Weather.js";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Main />}></Route>
-      <Route path="/weather" element={<Weather />}></Route>
+      <Route path="/" element={<Weather />}></Route>      
     </Routes>
   );
 }
